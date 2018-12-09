4088976b830439c3cc1a0b375995798ce47bee3f
Julian Currie <julian.currie@gmail.com>
2018-12-09 17:46:22 +0700

    use ListView builder to display pokemon list

--- lib/pokemon.dart
+++ lib/pokemon.dart
@@ -1,27 +1,31 @@
 import 'package:flutter/material.dart';
 
 class Pokemon extends StatelessWidget {
-  final List<String> pokemon;
+  final List<String> pokemonList;
 
-  Pokemon(this.pokemon);
+  Pokemon(this.pokemonList);
+
+  Widget _buildPokemonItem(BuildContext context, int index) {
+    return Card(
+      color: Theme.of(context).primaryColor,
+      margin: EdgeInsets.all(16),
+      child: Padding(
+        padding: EdgeInsets.all(16),
+        child: Row(
+          children: <Widget>[
+            Image.asset('assets/gengar.png'),
+            Text(pokemonList[index], style: TextStyle(color: Colors.white, fontSize: 22))
+          ],
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-        children: pokemon
-            .map((element) => Card(
-                color: Theme.of(context).primaryColor,
-                margin: EdgeInsets.all(16),
-                child: Padding(
-                  padding: EdgeInsets.all(16),
-                  child: Row(
-                    children: <Widget>[
-                      Image.asset('assets/gengar.png'),
-                      Text(element,
-                          style: TextStyle(color: Colors.white, fontSize: 22)),
-                    ],
-                  ),
-                )))
-            .toList());
+    return ListView.builder(
+      itemBuilder: _buildPokemonItem,
+      itemCount: pokemonList.length,
+    );
   }
 }
