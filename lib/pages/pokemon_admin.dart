df92d68bc31624b9ddf251994e28112bb2606234
Julian Currie <julian.currie@gmail.com>
2018-12-11 18:19:34 +0700

    added two tabs with icons to the app bar using DefaultTabController

--- lib/pages/pokemon_admin.dart
+++ lib/pages/pokemon_admin.dart
@@ -5,29 +5,46 @@ import './pokemon.dart';
 class PokemonAdminPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: Drawer(
-        child: Column(
-          children: <Widget>[
-            AppBar(
-              automaticallyImplyLeading: false,
-              title: Text("Julian Currie"),
-            ),
-            ListTile(
-              title: Text('All Pokemon'),
-              onTap: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(builder: (context) => Pokemon()),
-                );
-              },
-            )
-          ],
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        drawer: Drawer(
+          child: Column(
+            children: <Widget>[
+              AppBar(
+                automaticallyImplyLeading: false,
+                title: Text("Julian Currie"),
+              ),
+              ListTile(
+                title: Text('All Pokemon'),
+                onTap: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => Pokemon()),
+                  );
+                },
+              )
+            ],
+          ),
         ),
-      ),
-      appBar: AppBar(title: Text('Manage Pokemon')),
-      body: Center(
-        child: Text('Manage Your Pokemon'),
+        appBar: AppBar(
+          title: Text('Manage Pokemon'),
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(
+                icon: Icon(Icons.create),
+                text: "Create Pokemon",
+              ),
+              Tab(
+                icon: Icon(Icons.list),
+                text: "My Pokemon",
+              ),
+            ],
+          ),
+        ),
+        body: Center(
+          child: Text('Manage Your Pokemon'),
+        ),
       ),
     );
   }
