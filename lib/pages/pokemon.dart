deb74347692a245122c457c4d8ab4f86390857e4
Julian Currie <julian.currie@gmail.com>
2018-12-11 17:44:22 +0700

    Added side drawer to Pokemon page with a ListTile

--- lib/pages/pokemon.dart
+++ lib/pages/pokemon.dart
@@ -6,6 +6,20 @@ class Pokemon extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: Column(
+          children: <Widget>[
+            AppBar(
+              automaticallyImplyLeading: false,
+              title: Text("Julian Currie"),
+            ),
+            ListTile(
+              title: Text('Manage Pokemon'),
+              onTap: () {},
+            )
+          ],
+        ),
+      ),
       appBar: AppBar(
         title: Text("Pokemon"),
       ),
