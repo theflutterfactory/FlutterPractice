11f63a29c6a8363e8e7055d18a7132759b505587
Julian Currie <julian.currie@gmail.com>
2018-12-12 20:46:33 +0700

    set email and password state on auth page

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -1,46 +1,67 @@
 import 'package:flutter/material.dart';
 
-class AuthPage extends StatelessWidget {
+class AuthPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _AuthPageState();
+  }
+}
+
+class _AuthPageState extends State<AuthPage> {
+  String _email;
+  String _password;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("LOGIN"),
+      appBar: AppBar(
+        title: Text("LOGIN"),
+      ),
+      body: Container(
+        margin: EdgeInsets.all(16.0),
+        child: ListView(
+          children: <Widget>[
+            SizedBox(height: 32),
+            Text(
+              "Pokemon Manager",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 32,
+              ),
+            ),
+            SizedBox(height: 32),
+            TextField(
+              decoration: InputDecoration(labelText: "Email"),
+              keyboardType: TextInputType.emailAddress,
+              onChanged: (String value) {
+                setState(() {
+                  _email = value;
+                });
+              },
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: "Password"),
+              obscureText: true,
+              onChanged: (String value) {
+                setState(() {
+                  _password = value;
+                });
+              },
+            ),
+            SizedBox(height: 16),
+            RaisedButton(
+              child: Text(
+                "LOGIN",
+                style: TextStyle(color: Colors.white, fontSize: 16),
+              ),
+              color: Theme.of(context).accentColor,
+              onPressed: () {
+                Navigator.pushReplacementNamed(context, '/pokemon');
+              },
+            ),
+          ],
         ),
-        body: Container(
-          margin: EdgeInsets.all(16.0),
-          child: ListView(
-            children: <Widget>[
-              SizedBox(height: 32),
-              Text(
-                "Pokemon Manager",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 32,
-                ),
-              ),
-              SizedBox(height: 32),
-              TextField(
-                decoration: InputDecoration(labelText: "Email"),
-                keyboardType: TextInputType.emailAddress,
-              ),
-              TextField(
-                decoration: InputDecoration(labelText: "Password"),
-                obscureText: true,
-              ),
-              SizedBox(height: 16),
-              RaisedButton(
-                child: Text(
-                  "Login",
-                  style: TextStyle(color: Colors.white, fontSize: 16),
-                ),
-                color: Theme.of(context).accentColor,
-                onPressed: () {
-                  Navigator.pushReplacementNamed(context, '/pokemon');
-                },
-              ),
-            ],
-          ),
-        ));
+      ),
+    );
   }
 }
