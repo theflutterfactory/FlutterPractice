6bf66c5292eb45e32b6409ee52f5f2d759cf1d0c
Julian Currie <julian.currie@gmail.com>
2018-12-12 18:23:09 +0700

    added Textfields for pokemon name, description, type and starting health

--- lib/pages/pokemon_create.dart
+++ lib/pages/pokemon_create.dart
@@ -1,19 +1,53 @@
 import 'package:flutter/material.dart';
 
-class PokemonCreatePage extends StatelessWidget {
+class PokemonCreatePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _PokemonCreatePageState();
+  }
+}
+
+class _PokemonCreatePageState extends State<PokemonCreatePage> {
+  String name = '';
+  String description = '';
+  String type = '';
+  double startingHealth;
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: RaisedButton(
-        child: Text("Save"),
-        onPressed: () {
-          showModalBottomSheet(
-              context: context,
-              builder: (context) {
-                return Center(child: Text("Modal for Pokemon creation"));
-              });
-        },
-      ),
+    return Column(
+      children: <Widget>[
+        TextField(
+          onChanged: (String value) {
+            setState(() {
+              name = value;
+            });
+          },
+        ),
+        TextField(
+          maxLines: 4,
+          onChanged: (String value) {
+            setState(() {
+              description = value;
+            });
+          },
+        ),
+        TextField(
+          onChanged: (String value) {
+            setState(() {
+              type = value;
+            });
+          },
+        ),
+        TextField(
+          keyboardType: TextInputType.number,
+          onChanged: (String value) {
+            setState(() {
+              startingHealth = double.parse(value);
+            });
+          },
+        ),
+      ],
     );
   }
 }
