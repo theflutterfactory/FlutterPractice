5a4350e201e8d609baeff7607bdb462453332419
Julian Currie <julian.currie@gmail.com>
2018-12-20 17:31:45 +0700

    display Pokemon list in owned Pokemon page

--- lib/pages/pokemon_owned.dart
+++ lib/pages/pokemon_owned.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
 
+import '../data/pokemon.dart';
+
 class PokemonOwnedPage extends StatelessWidget {
+  final List<Pokemon> pokemonList;
+
+  PokemonOwnedPage(this.pokemonList);
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Your Pokemon'),
+    return ListView.builder(
+      itemBuilder: (BuildContext context, int index) {
+        return ListTile(
+          contentPadding: EdgeInsets.all(16),
+          leading: Image.asset(
+            pokemonList[index].image,
+            height: 80,
+          ),
+          title: Text(pokemonList[index].name),
+          trailing: IconButton(
+            icon: Icon(Icons.edit),
+            onPressed: () {},
+          ),
+        );
+      },
+      itemCount: pokemonList.length,
     );
   }
 }
